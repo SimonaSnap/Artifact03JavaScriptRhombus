8a315aa2ed2a05dd7b5eb30b8eb6f0caf52dd562
Simona <simona_s@outlook.com>
2023-02-10 15:34:38 -0800

    brackets

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ for (i=0;i<pSideM;i++){
 for(j=0;j<=i;j++){
 
 //Is the position even or odd so we change the color
-if (j%2)
 if (j%2){
     sideM.style.color = pColorChoice1;
     sideM.textContent = pSymbol;
@@ -25,10 +24,12 @@ else {
     sideM.style.color = pColorChoice2;
     sideM.textContent = pSymbol
 }
+}
 
 var uR = document.getElementById("upRight")
 uR.append(sideM);
 
+}
 }
 
 function downRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
@@ -48,13 +49,14 @@ else {
     sideM.style.color = pColorChoice2;
     sideM.textContent = pSymbol
 }
-console.log(sideM);
+//console.log(sideM);
 
 }
 
 
 var dR = document.getElementById("upRight")
 dR.append(sideM);
+}
 }
 
 create.onclick = function createRhombus(pSideM, pColorChoice1, pColorChoice2, pSymbol) {
