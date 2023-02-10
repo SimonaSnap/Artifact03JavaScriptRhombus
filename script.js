0788ff3ca9d071c857f25754dc56c5d3d7f3d6b2
Simona <simona_s@outlook.com>
2023-02-10 15:43:30 -0800

    cleanup

--- script.js
+++ script.js
@@ -9,54 +9,49 @@ var pColorChoice2 = document.getElementById('colorChoice2').value;
 var pSymbol = document.getElementById('symbol').value;
 
 function upRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
-var sideM = document.createElement("p");
-sideM.textContent = "";
-for (i=0;i<pSideM;i++){
-//Create each line on the Rhombus
-for(j=0;j<=i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2){
-    sideM.style.color = pColorChoice1;
-    sideM.textContent = pSymbol;
-}
-else {
-    sideM.style.color = pColorChoice2;
-    sideM.textContent = pSymbol
-}
-}
-
-var uR = document.getElementById("upRight")
-uR.append(sideM);
-
-}
+    var sideM = document.createElement("span");
+    for (i=0;i<pSideM;i++){
+        //Create each line on the Rhombus
+        for(j=0;j<=i;j++){
+            //Is the position even or odd so we change the color
+            if (j%2){
+                sideM.style.color = pColorChoice1;
+                sideM.textContent = pSymbol;
+            }
+            else {
+                sideM.style.color = pColorChoice2;
+                sideM.textContent = pSymbol;
+            }
+            //console.log(sideM);
+        }
+        var uR = document.getElementById("upRight")
+        uR.append(sideM);
+        var dL = document.getElementById("downLeft");
+        dL.append(sideM);
+    }
 }
 
 function downRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
-var sideM ="";
-for (i=sideM;i > 0;i--){
-sideM = document.createElement("span"); 
-sideM.textContent = "";
-//Create each line on the Rhombus
-for(j=0;j<i;j++){
-
-//Is the position even or odd so we change the color
-if (j%2){
-    sideM.style.color = pColorChoice1;
-    sideM.textContent = pSymbol;
-}
-else {
-    sideM.style.color = pColorChoice2;
-    sideM.textContent = pSymbol
-}
-//console.log(sideM);
-
-}
-
-
-var dR = document.getElementById("upRight")
-dR.append(sideM);
-}
+    var sideM = document.createElement("span"); 
+    for (i=psideM;i > 0;i--){
+        //Create each line on the Rhombus
+        for(j=0;j<i;j++){
+            //Is the position even or odd so we change the color
+            if (j%2){
+                sideM.style.color = pColorChoice1;
+                sideM.textContent = pSymbol;
+            }
+            else {
+                sideM.style.color = pColorChoice2;
+                sideM.textContent = pSymbol
+            }
+            //console.log(sideM);
+        }
+        var dR = document.getElementById("downRight");
+        dR.append(sideM);
+        var uL = document.getElementById("upLeft");
+        uL.append(sideM);
+    }
 }
 
 create.onclick = function createRhombus(pSideM, pColorChoice1, pColorChoice2, pSymbol) {
