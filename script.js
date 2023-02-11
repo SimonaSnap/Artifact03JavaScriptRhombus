f911d945d2d5ea971e648232485c7e2eb5e799d6
Simona <simona_s@outlook.com>
2023-02-10 16:19:41 -0800

    function scrub

--- script.js
+++ script.js
@@ -19,19 +19,14 @@ function upRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
     var uR = document.getElementById("upRight")
     var dL = document.getElementById("downLeft");
     console.log(sideM);
-    for (i=0;i<pSideM;i++){
-        //Create each line on the Rhombus
-        for(j=0;j<=i;j++){
-            //Is the position even or odd so we change the color
-            if (j%2){
-                sideM.style.color = pColorChoice1;
-                sideM.textContent = pSymbol;
-            }
-            else {
-                sideM.style.color = pColorChoice2;
-                sideM.textContent = pSymbol;
-            }
-            console.log(sideM);
+    for (i=0; i<pSideM; i++) {
+        if(i%2){
+            sideM.style.backgroundColor = pColorChoice1;
+            sideM.textContent = pSymbol;
+        }
+        else {
+            sideM.style.backgroundColor = pColorChoice2;
+            sideM.textContent = pSymbol;
         }
         uR.append(sideM);
         dL.append(sideM);
@@ -42,21 +37,15 @@ function downRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
     var sideM = document.createElement("span"); 
     var dR = document.getElementById("downRight");
     var uL = document.getElementById("upLeft");
-    for (i=pSideM;i > 0;i--){
-        //Create each line on the Rhombus
-        for(j=0;j<i;j++){
-            //Is the position even or odd so we change the color
-            if (j%2){
-                sideM.style.color = pColorChoice1;
-                sideM.textContent = pSymbol;
-            }
-            else {
-                sideM.style.color = pColorChoice2;
-                sideM.textContent = pSymbol
-            }
-            console.log(sideM);
+    for (i=0; i<pSideM; i++) {
+        if(i%2){
+            sideM.style.backgroundColor = pColorChoice1;
+            sideM.textContent = pSymbol;
         }
-        
+        else {
+            sideM.style.backgroundColor = pColorChoice2;
+            sideM.textContent = pSymbol;
+        }
         dR.append(sideM);
         uL.append(sideM);
     }
