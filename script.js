f4552dbb2947e2c75be3e0fb802e6dfa1835246e
Simona <simona_s@outlook.com>
2023-02-10 22:34:21 -0800

    order?

--- script.js
+++ script.js
@@ -12,12 +12,6 @@ var dL = document.getElementById("downLeft");
 var dR = document.getElementById("downRight");
 var uL = document.getElementById("upLeft");
 
-create.onclick = function createRhombus(pSideM, pColorChoice1, pColorChoice2, pSymbol) {
-    //console.log("clicked");
-    upRight(pSideM, pColorChoice1, pColorChoice2, pSymbol);
-    downRight(pSideM, pColorChoice1, pColorChoice2, pSymbol);
-}
-
 function upRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
     var sideM = document.createElement("span");
     console.log(sideM);
@@ -25,16 +19,14 @@ function upRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
         if(i%2 == 0){
             sideM.style.backgroundColor = pColorChoice1;
             sideM.textContent = pSymbol;
-            uR.appendChild(sideM);
-            dL.appendChild(sideM);
         }
         else {
             sideM.style.backgroundColor = pColorChoice2;
             sideM.textContent = pSymbol;
-            uR.appendChild(sideM);
-            dL.appendChild(sideM);
             console.log(sideM);
         }
+        uR.appendChild(sideM);
+        dL.appendChild(sideM);
     }
 }
 
@@ -45,15 +37,19 @@ function downRight(pSideM, pColorChoice1, pColorChoice2, pSymbol){
         if(i%2 == 0){
             sideM.style.backgroundColor = pColorChoice1;
             sideM.textContent = pSymbol;
-            dR.appendChild(sideM);
-            uL.appendChild(sideM);
+            
         }
         else {
             sideM.style.backgroundColor = pColorChoice2;
             sideM.textContent = pSymbol;
-            dR.appendChild(sideM);
-            uL.appendChild(sideM);
         }
+        dR.appendChild(sideM);
+        uL.appendChild(sideM);
     }
 }
 
+create.onclick = function createRhombus(pSideM, pColorChoice1, pColorChoice2, pSymbol) {
+    //console.log("clicked");
+    upRight(pSideM, pColorChoice1, pColorChoice2, pSymbol);
+    downRight(pSideM, pColorChoice1, pColorChoice2, pSymbol);   
+}
